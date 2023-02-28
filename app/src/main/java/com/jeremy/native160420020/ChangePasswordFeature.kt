4ac39499244f6a518ac8da26aea6ab07fd6330df
jkgpro <jeremykage@gmail.com>
2023-02-28 17:49:29 +0700

    Update activity change password

---
+++ app/src/main/java/com/jeremy/native160420020/ChangePasswordFeature.kt
@@ -0,0 +1,11 @@
+package com.jeremy.native160420020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePasswordFeature : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password_feature)
+    }
+}
