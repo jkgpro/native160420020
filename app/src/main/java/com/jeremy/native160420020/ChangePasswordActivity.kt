749cca9f826dbb24a2dfbe71536b584c6f6e2c88
jkgpro <jeremykage@gmail.com>
2023-02-28 17:34:08 +0700

    tambah activity change password =

--- app/src/main/java/com/jeremy/native160420020/ChangePasswordActivity.kt
+++ app/src/main/java/com/jeremy/native160420020/ChangePasswordActivity.kt
@@ -3,12 +3,9 @@ package com.jeremy.native160420020
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ChangePasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // todo: bikin halaman login
-
+        setContentView(R.layout.activity_change_password)
     }
 }
